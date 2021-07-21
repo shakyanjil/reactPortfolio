4e186a8d7675b0cacd71073f0f10ce242baa3aff
shakyanjil <shakyanjil13@gmail.com>
2021-07-21 13:31:05 +0545

    side bar

---
+++ src/components/SidebarData.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import * as AiIcons from 'react-icons/ai';
+import * as FaIcons from 'react-icons/fa';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'About',
+        path: '/about',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Resume',
+        path: '/resume',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Portfolio',
+        path: '/portfolio',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Contact',
+        path: '/contact',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    }
+]
