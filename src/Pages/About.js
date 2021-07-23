b77b595d4f50f2124e4b1260082ba8de8814d2ec
shakyanjil <shakyanjil13@gmail.com>
2021-07-23 18:19:19 +0545

    started about me page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import "./about.css";
+import banner from "../images/banner.png";
 
 function About() {
   return (
-    <div className='row'>
-
-      <h1>About</h1>
-      
+    <div className="container">
+      <div className="banner">
+        <img src={banner} alt="banner image" />
+      </div>
+      <div className="aboutme">
+        <div className="myself">
+          <h3>Anjil shakya</h3>
+          <p>
+            This is abvout myself. hello nice to meet you . welcome to my
+            portfolio. i built this using react js.
+          </p>
+        </div>
+        <div className="details">
+          <ul>
+            <li>Date of Birth: 21/03/1998 </li>
+            <li>Date of Birth: 21/03/1998 </li>
+            <li>Date of Birth: 21/03/1998 </li>
+            <li>Date of Birth: 21/03/1998 </li>
+            <li>Date of Birth: 21/03/1998 </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Label.js
+++ src/components/Label.js
@@ -3,9 +3,10 @@ import "./label.css";
 import { SidebarData } from "./SidebarData";
 
 function Label() {
-  SidebarData.map((item, index) => {
-    return <h4 key={index}>{item.title}</h4>;
-  });
+  return <h4>LABEL</h4>;
+  // SidebarData.map((item, index) => {
+  //   return <h4 key={index}>{item.title}</h4>;
+  // });
 }
 
 export default Label;
